c807cd468a7c53e03cfea22f3a7db152c562f23c
Justin Searls <searls@gmail.com>
2011-08-30 18:07:07 -0400

    added jasmine css routes.. .still a mess

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ require 'jasmine-core'
 JasmineRails::Engine.routes.draw do
   root :to => "spec_runner#index"
   
-  Jasmine::Core.js_files.map do |f| 
+  Jasmine::Core.js_files.each do |f| 
     mount Rack::File.new("/#{Jasmine::Core.path}/#{f}"), :at => "lib/#{f}"
   end
+  
+  Jasmine::Core.css_files.each do |f|
+    mount Rack::File.new("/#{Jasmine::Core.path}/#{f}"), :at => "css/#{f}"
+  end
+  
 end
