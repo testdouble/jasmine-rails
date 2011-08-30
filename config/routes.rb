027b2a16e955563c283b2defd6886b126613a381
Justin Searls <searls@gmail.com>
2011-08-30 18:16:50 -0400

    mounted user's css

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ JasmineRails::Engine.routes.draw do
   root :to => "spec_runner#index"
   
   Jasmine::Core.js_files.each do |f| 
-    mount Rack::File.new("/#{Jasmine::Core.path}/#{f}"), :at => "lib/#{f}"
+    mount Rack::File.new("/#{Jasmine::Core.path}/#{f}"), :at => "lib/js/#{f}"
   end
   
   Jasmine::Core.css_files.each do |f|
-    mount Rack::File.new("/#{Jasmine::Core.path}/#{f}"), :at => "css/#{f}"
+    mount Rack::File.new("/#{Jasmine::Core.path}/#{f}"), :at => "lib/css/#{f}"
+  end
+  
+  Jasmine::Config.new.css_files.each do |f|
+    mount Rack::File.new("/#{Rails.root}/#{f}"), :at => "css/#{f}"
   end
   
 end
