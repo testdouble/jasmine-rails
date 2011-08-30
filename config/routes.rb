b5cbc5621baeac0d61588ce450e9d32194499052
Justin Searls <searls@gmail.com>
2011-08-30 17:30:39 -0400

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 JasmineRails::Engine.routes.draw do
   root :to => "spec_runner#index"
+  
+  Jasmine::Core.js_files.map do |f| 
+    mount Rack::File.new("/#{Jasmine::Core.path}/#{f}"), :at => "lib/#{f}"
+  end
 end
