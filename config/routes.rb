7a89a2ea56dc985c6faa7034b0c5a075d30d8feb
Justin Searls <searls@gmail.com>
2011-08-30 17:19:12 -0400

    experimenting with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+require 'jasmine-core'
+require 'jasmine'
+
 JasmineRails::Engine.routes.draw do
   root :to => "spec_runner#index"
+  
+  Jasmine::Core.js_files.map do |f| 
+    map "lib/#{f}"
+      Rack::File.new("/#{Jasmine::Core.path}/#{f}")
+    end
+  end 
 end
