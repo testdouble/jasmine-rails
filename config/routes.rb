8aefc76421cbe6729b78304e7d691c5cf81b4f40
Justin Searls <searls@gmail.com>
2011-08-30 17:26:57 -0400

    fail again

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
-require 'jasmine-core'
-
 JasmineRails::Engine.routes.draw do
   root :to => "spec_runner#index"
-  
-  debugger
-  Jasmine::Core.js_files.map do |f| 
-    map "lib/#{f}" do
-      Rack::File.new("/#{Jasmine::Core.path}/#{f}")
-    end
-  end 
 end
