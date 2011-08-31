42b4f938acf3454e58b20c493edb9519ba24e015
Justin Searls <searls@gmail.com>
2011-08-31 14:06:13 -0400

    ripped the unnecessary routes out of the routes.rb, removed the favicon.

--- config/routes.rb
+++ config/routes.rb
@@ -3,26 +3,4 @@ require 'jasmine'
 
 JasmineRails::Engine.routes.draw do
   root :to => "spec_runner#index"
-  
-  Jasmine::Core.js_files.each do |f| 
-    mount Rack::File.new("/#{Jasmine::Core.path}/#{f}"), :at => "lib/js/#{f}"
-  end
-  
-  Jasmine::Core.css_files.each do |f|
-    mount Rack::File.new("/#{Jasmine::Core.path}/#{f}"), :at => "lib/css/#{f}"
-  end
-  
-  jasmine_config = Jasmine::Config.new
-
-  jasmine_config.css_files.each do |f|
-    mount Rack::File.new("#{Rails.root}#{f}"), :at => "css/#{f}"
-  end
-
-  jasmine_config.js_files.each do |f|
-    mount Rack::File.new("#{Rails.root}#{f}"), :at => "js/#{f}"
-  end
-    
-  mount Rack::File.new(jasmine_config.spec_dir), :at => "js/"+jasmine_config.spec_path
-  mount Rack::File.new(jasmine_config.project_root), :at => jasmine_config.root_path
-  mount Rack::File.new(Jasmine.root), :at => '/__JASMINE_ROOT__'
 end
