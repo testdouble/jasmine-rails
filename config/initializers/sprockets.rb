8a78020befe9d31be599a2244e1e5ff23898738e
Justin Searls <searls@gmail.com>
2011-08-31 19:29:11 -0400

    extracted

--- config/initializers/sprockets.rb
+++ config/initializers/sprockets.rb
@@ -1,4 +1,6 @@
 require 'processes_jasmine_directives'
 
-Rails.application.assets.register_preprocessor 'application/javascript', ProcessesJasmineDirectives
-Rails.application.assets.register_preprocessor 'text/css', ProcessesJasmineDirectives
+assets = Rails.application.assets
+
+assets.register_preprocessor 'application/javascript', ProcessesJasmineDirectives
+assets.register_preprocessor 'text/css', ProcessesJasmineDirectives
