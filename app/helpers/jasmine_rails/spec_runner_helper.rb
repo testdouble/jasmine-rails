66fe666b76373bc7671b878c4daeac8ade43bd76
Ryan Sonnek <ryan.sonnek@gmail.com>
2013-02-27 11:58:04 -0600

    manually build asset for specs to filter out duplicate dependencies

--- app/helpers/jasmine_rails/spec_runner_helper.rb
+++ app/helpers/jasmine_rails/spec_runner_helper.rb
@@ -12,28 +12,11 @@ module JasmineRails
       if params[:console]
         files += %w{ jasmine-console-reporter.js }
       end
-      files += filter_files JasmineRails.spec_dir, JasmineRails.jasmine_config['helpers']
       files
     end
 
     def spec_js_files
-      files = filter_files JasmineRails.spec_dir, JasmineRails.jasmine_config['spec_files']
-      if filter = params[:spec].to_s.split.first
-        files = files.select do |file|
-          file.include? filter
-        end
-      end
-      files
-    end
-
-    private
-    def filter_files(root_dir, patterns)
-      files = patterns.to_a.collect do |pattern|
-        Dir.glob(root_dir.join(pattern))
-      end
-      files = files.flatten
-      files = files.collect {|f| f.gsub(root_dir.to_s + '/', '') }
-      files || []
+      %w{ jasmine-specs.js }
     end
   end
 end
--- lib/jasmine-rails.rb
+++ lib/jasmine-rails.rb
@@ -13,5 +13,19 @@ module JasmineRails
         YAML.load_file(path)
       end
     end
+
+    def spec_files
+      filter_files JasmineRails.spec_dir, JasmineRails.jasmine_config['spec_files']
+    end
+
+    private
+    def filter_files(root_dir, patterns)
+      files = patterns.to_a.collect do |pattern|
+        Dir.glob(root_dir.join(pattern))
+      end
+      files = files.flatten
+      files = files.collect {|f| f.gsub(root_dir.to_s + '/', '') }
+      files || []
+    end
   end
 end
