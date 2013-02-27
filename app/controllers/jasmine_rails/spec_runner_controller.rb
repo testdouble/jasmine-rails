25dd5b4c8c8f9e301e8932fcdc5e26ec4c914378
Ryan Sonnek <ryan.sonnek@gmail.com>
2013-02-27 15:46:59 -0600

    remove unused config

--- app/controllers/jasmine_rails/spec_runner_controller.rb
+++ app/controllers/jasmine_rails/spec_runner_controller.rb
@@ -1,7 +1,6 @@
 module JasmineRails
   class SpecRunnerController < ApplicationController
     def index
-      params[:debug_assets] = true unless params[:console]
     end
   end
 end
--- lib/jasmine_rails/engine.rb
+++ lib/jasmine_rails/engine.rb
@@ -6,7 +6,6 @@ module JasmineRails
 
     initializer :assets do |config|
       Rails.application.config.assets.paths << Jasmine::Core.path
-      Rails.application.config.assets.paths << Jasmine.config.boot_dir
       Rails.application.config.assets.paths << JasmineRails.spec_dir
     end
   end
