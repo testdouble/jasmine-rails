28242c475049e8d286343e27539fb5eb005ff145
Ryan Sonnek <ryan.sonnek@gmail.com>
2013-03-14 10:06:14 -0500

    automatically flush jasmine-specs asset cache when new spec files/directories are added

--- lib/jasmine-rails.rb
+++ lib/jasmine-rails.rb
@@ -18,7 +18,22 @@ module JasmineRails
       filter_files JasmineRails.spec_dir, JasmineRails.jasmine_config['spec_files']
     end
 
+    # iterate over all spec dirs (including subdirectories)
+    def each_spec_dir(&block)
+      each_dir spec_dir.to_s, &block
+    end
+
     private
+
+    def each_dir(root, &block)
+      yield root
+      Dir[root + '/*'].each do |file|
+        if File.directory?(file)
+          each_dir(file, &block)
+        end
+      end
+    end
+
     def filter_files(root_dir, patterns)
       files = patterns.to_a.collect do |pattern|
         Dir.glob(root_dir.join(pattern))
