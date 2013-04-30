bb79f57fdce030cf4cb5dc905906a7dc80644899
Irfan Shah <mail.urfi@gmail.com>
2013-04-30 09:09:48 +0530

    Adding Support for Rails 4

--- lib/jasmine_rails/offline_asset_paths.rb
+++ lib/jasmine_rails/offline_asset_paths.rb
@@ -7,24 +7,23 @@ module JasmineRails
     mattr_accessor :disabled
     extend ActiveSupport::Concern
     included do
+      alias_method :compute_public_path, :compute_asset_path
+      alias_method :compute_asset_path_with_offline_asset, :compute_public_path_with_offline_asset
       alias_method_chain :compute_public_path, :offline_asset
+      alias_method_chain :compute_asset_path, :offline_asset
     end
 
     def compute_public_path_with_offline_asset(source, dir, options={})
       return compute_public_path_without_offline_asset(source, dir, options) if JasmineRails::OfflineAssetPaths.disabled
       return source if source.starts_with?('/')
       content = Rails.application.assets[source].to_s
-      source_path = offline_asset_dir.join(source)
+      source_path = Rails.root.join('spec/tmp/assets').join(source)
 
       FileUtils.mkdir_p File.dirname(source_path)
       Rails.logger.debug "Compiling #{source} to #{source_path}"
       File.open(source_path, 'w') {|f| f << content }
-      "assets/#{source}"
+      "/assets/#{source}"
     end
 
-    private
-    def offline_asset_dir
-      Rails.root.join('spec/tmp/assets')
-    end
   end
 end
