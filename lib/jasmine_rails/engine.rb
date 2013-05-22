7876ec90c6616dc31956a448c457b15519ca5a55
Simon Mathieu <simon.math@gmail.com>
2013-05-22 10:58:35 -0700

    Use the application's assets.debug config

--- lib/jasmine_rails/engine.rb
+++ lib/jasmine_rails/engine.rb
@@ -5,7 +5,6 @@ module JasmineRails
     isolate_namespace JasmineRails
 
     initializer :assets do |config|
-      Rails.application.config.assets.debug = false
       Rails.application.config.assets.paths << Jasmine::Core.path
       Rails.application.config.assets.paths << JasmineRails.spec_dir
     end
