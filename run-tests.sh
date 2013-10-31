eb76a0f5343bbabff153ce1e15c62fe8a4436444
Justin Searls <searls@gmail.com>
2013-10-31 15:06:53 -0400

    properly installs the gems for each gemfile first

--- run-tests.sh
+++ run-tests.sh
@@ -1,17 +1,29 @@
 #!/bin/bash -e
 
-function run_tests(){
+function install_gems(){
   local version=$1
   export BUNDLE_GEMFILE="gemfiles/rails-$version"
   bundle install
+}
+export -f install_gems
+
+function run_tests(){
+  local version=$1
+  export BUNDLE_GEMFILE="gemfiles/rails-$version"
   rake testbed:current:all
 }
 export -f run_tests
 
 bundle install
+
+install_gems "3.1"
+install_gems "3.2"
+install_gems "4.0"
+
 mkdir -p spec/testbeds
+export BUNDLE_GEMFILE=""
 bundle exec generate-testbeds
 
+run_tests "3.1"
 run_tests "3.2"
 run_tests "4.0"
-run_tests "3.1"
