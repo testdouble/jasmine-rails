5dc099ec9b9d484150feb3f361ae299eb5306c31
randoum <randoum@gmail.com>
2014-04-24 11:34:01 +0800

    spec_dir accept array of directories

--- lib/jasmine_rails/engine.rb
+++ lib/jasmine_rails/engine.rb
@@ -6,7 +6,9 @@ module JasmineRails
 
     initializer :assets do |config|
       Rails.application.config.assets.paths << Jasmine::Core.path
-      Rails.application.config.assets.paths << JasmineRails.spec_dir
+      JasmineRails.spec_dir.each do |dir|
+        Rails.application.config.assets.paths << dir
+      end
       Rails.application.config.assets.precompile += %w(jasmine.css boot.js jasmine-boot.js json2.js jasmine.js jasmine-html.js jasmine-console-shims.js jasmine-console-reporter.js jasmine-specs.js)
     end
   end
