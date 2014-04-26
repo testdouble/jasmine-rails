dd2b01a8155b41dae958160b9c05528ee1831e09
Justin Searls <searls@gmail.com>
2014-04-26 16:51:18 -0400

    Fixes #117

--- lib/jasmine_rails/engine.rb
+++ lib/jasmine_rails/engine.rb
@@ -7,7 +7,7 @@ module JasmineRails
     initializer :assets do |config|
       Rails.application.config.assets.paths << Jasmine::Core.path
       Rails.application.config.assets.paths << JasmineRails.spec_dir
-      Rails.application.config.assets.precompile += %w(jasmine.css boot.js jasmine-boot.js json2.js jasmine.js jasmine-html.js jasmine-console-shims.js jasmine-console-reporter.js jasmine-specs.js)
+      Rails.application.config.assets.precompile += %w(jasmine.css boot.js jasmine-boot.js json2.js jasmine.js jasmine-html.js jasmine-console-shims.js jasmine-console-reporter.js jasmine-specs.js jasmine-specs.css)
     end
   end
 end
