cc973376591dae9c68ee9a732348531bd1d49d49
randoum <randoum@gmail.com>
2014-08-04 09:33:30 +0800

    Update engine.rb

--- lib/jasmine_rails/engine.rb
+++ lib/jasmine_rails/engine.rb
@@ -6,7 +6,6 @@ module JasmineRails
 
     initializer :assets do |config|
       [Jasmine::Core.path, JasmineRails.include_dir, JasmineRails.spec_dir].flatten.compact.each do |dir|
-        puts dir
         Rails.application.config.assets.paths << dir
       end
       Rails.application.config.assets.precompile += %w(jasmine.css boot.js jasmine-boot.js json2.js jasmine.js jasmine-html.js jasmine-console-shims.js jasmine-console-reporter.js jasmine-specs.js jasmine-specs.css)
