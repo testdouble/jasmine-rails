6b387249afeabb29f706ab2d4bb16ddb46b0659a
fabrizio <fabrizio.machella@gmail.com>
2017-09-04 21:55:40 +0200

    Revert "added rails root path to window in order to use in jasmine jquery path"
    This reverts commit eb31d320055ab37fa9b28dfc24eb7d1ec64b85e4.

--- app/helpers/jasmine_rails/spec_runner_helper.rb
+++ app/helpers/jasmine_rails/spec_runner_helper.rb
@@ -26,10 +26,6 @@ module JasmineRails
       files << 'jasmine-specs.js'
       files
     end
-    
-    def rails_root_path
-      javascript_tag "window.root_path='#{Rails.root}'"
-    end
 
     def jasmine_boot_file
       if jasmine2?
